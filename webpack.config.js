3a0744f7fb071105a908234031a8caea0b2ed838
Luke Nzang'u <saintlukke@gmail.com>
2018-03-08 15:59:13 +0300

    Add html loader, file loader,clean webpack plugin and html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 var path = require('path');
 var extractTextPlugin = require('extract-text-webpack-plugin');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var cleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new extractTextPlugin({
-    filename: 'main.css',
-    allChunks: true
+    filename: 'main.css'
 })
 module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist'
+    // publicPath: '/dist'
     },
     module: {
         rules: [
@@ -30,10 +31,31 @@ module.exports = {
                 use: extractPlugin.extract({
                     use: ['css-loader', 'sass-loader']
                 })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use:[
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins: [
-        extractPlugin
+        extractPlugin,
+        new htmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new cleanWebpackPlugin(['dist'])
     ]
 };
