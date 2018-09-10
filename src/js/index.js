1e8c0fe0896365f185785acf992aae3314bc6e29
Luda One <saintlukke@gmail.com>
2018-09-10 14:22:03 +0300

    Add some basic styling

--- src/js/index.js
+++ src/js/index.js
@@ -2,44 +2,6 @@ import '../scss/default.scss';
 import '../scss/client_home.scss';
 import '../menu.html';
 
-// import {includeHTML} from './includeHtml';
-
-const clientLoginDiv = document.querySelector('#client-form');
-const providerLoginDiv = document.querySelector('#provider-form');
-
-const clientButton = document.querySelector('#client-login');
-const providerButton = document.querySelector('#provider-login');
-
-var toggleClient = () =>{
-    console.log('clientButton clicked');
-    console.log('style is :: '+ clientLoginDiv.style.display);
-    if (clientLoginDiv.style.display === 'none'|| providerLoginDiv.style.display === ''){
-        console.log('was none');
-        clientLoginDiv.style.display = 'block';
-        providerLoginDiv.style.display = 'none';
-
-    }else {
-        clientLoginDiv.style.display = 'none';
-    }
-};
-var toggleProvider = () =>{
-    console.log('providerButton clicked');
-    console.log('style is :: '+ clientLoginDiv.style.display);
-
-    if (providerLoginDiv.style.display === 'none' || providerLoginDiv.style.display === ''){
-        console.log('was none');
-        providerLoginDiv.style.display = 'block';
-        clientLoginDiv.style.display = 'none';
-    }else {
-        providerLoginDiv.style.display = 'none';
-    }
-};
-
-clientButton.addEventListener('click',toggleClient);
-providerButton.addEventListener('click',toggleProvider);
-
-const clientLoginButton = document.querySelector('#cli_login_button');
-const providerLoginButton = document.querySelector('#pro_login_button');
 
 //Slideshow script
 var slideIndex = 1;
--- src/js/provider_register.js
+++ src/js/provider_register.js
@@ -1,7 +1,8 @@
-import firebase from "firebase/app";
-import 'firebase/auth';
-
-import firebaseui from "firebaseui";
+// import firebase from "firebase/app";
+// import 'firebase/auth';
+//
+// import firebaseui from "firebaseui";
+// import '../scss/provider_reg.scss';
 
 window.onload = () => {
 
@@ -15,51 +16,51 @@ window.onload = () => {
     // require("firebase/messaging");
     // require("firebase/functions");
 
-    var config = {
-      apiKey: "AIzaSyDX6gLOCC5GdVoF1Q9Ni0SII-s8OjAWKTQ",
-      authDomain: "chores-f5f4c.firebaseapp.com",
-      databaseURL: "https://chores-f5f4c.firebaseio.com",
-      projectId: "chores-f5f4c",
-      storageBucket: "chores-f5f4c.appspot.com",
-      messagingSenderId: "881509457100"
-    };
-
-    firebase.initializeApp(config);
-
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
-    var uiConfig = {
-      callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-          // User successfully signed in.
-          // Return type determines whether we continue the redirect automatically
-          // or whether we leave that to developer to handle.
-          return true;
-        },
-        uiShown: function() {
-          // The widget is rendered.
-          // Hide the loader.
-          document.getElementById('loader').style.display = 'none';
-        }
-      },
-      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-      signInFlow: 'popup',
-      signInSuccessUrl: 'http://localhost:8080/providerHome.html',
-      signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        // firebase.auth.PhoneAuthProvider.PROVIDER_ID
-      ],
-      // Terms of service url.
-      // tosUrl: '<your-tos-url>',
-      // Privacy policy url.
-      // privacyPolicyUrl: '<your-privacy-policy-url>'
-    };
-    ui.start('#firebasui', uiConfig);
+    // var config = {
+    //   apiKey: "AIzaSyDX6gLOCC5GdVoF1Q9Ni0SII-s8OjAWKTQ",
+    //   authDomain: "chores-f5f4c.firebaseapp.com",
+    //   databaseURL: "https://chores-f5f4c.firebaseio.com",
+    //   projectId: "chores-f5f4c",
+    //   storageBucket: "chores-f5f4c.appspot.com",
+    //   messagingSenderId: "881509457100"
+    // };
+    //
+    // firebase.initializeApp(config);
+    //
+    // var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    //
+    // var uiConfig = {
+    //   callbacks: {
+    //     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+    //       // User successfully signed in.
+    //       // Return type determines whether we continue the redirect automatically
+    //       // or whether we leave that to developer to handle.
+    //       return true;
+    //     },
+    //     uiShown: function() {
+    //       // The widget is rendered.
+    //       // Hide the loader.
+    //       document.getElementById('loader').style.display = 'none';
+    //     }
+    //   },
+    //   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    //   signInFlow: 'popup',
+    //   signInSuccessUrl: 'http://localhost:8080/providerHome.html',
+    //   signInOptions: [
+    //     // Leave the lines as is for the providers you want to offer your users.
+    //     // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //     // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //     // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //     // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    //     firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    //     // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    //   ],
+    //   // Terms of service url.
+    //   // tosUrl: '<your-tos-url>',
+    //   // Privacy policy url.
+    //   // privacyPolicyUrl: '<your-privacy-policy-url>'
+    // };
+    // ui.start('#firebasui', uiConfig);
 };
 
 
